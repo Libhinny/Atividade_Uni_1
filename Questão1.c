c8eeb6468692b8d992f30b4c5dbf06f2f82c28b1
Libhinny <Libhinny@gmail.com>
2023-07-05 15:03:45 -0300

    Questao1

--- Questão1.c
+++ Questão1.c
@@ -12,4 +12,5 @@ int main (void){
     printf ("Valor de x = %d \n Valor de y = %d \n Valor *p = %p", x, y, *p); 
     // O valor de x = 3, y = 4 e o ponteiro é o endereço da célula
     return 0;
+
 }
